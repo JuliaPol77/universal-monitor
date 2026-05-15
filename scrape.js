8dd2a04dd51730ab4a5896afd014d6af4686571c
JuliaPol77 <166243330+JuliaPol77@users.noreply.github.com>
2026-05-15 10:23:07 +0300

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -17,3 +17,18 @@ async function readKeywords() {
 const keywords = await readKeywords();
 
 console.log("KEYWORDS:", keywords);
+
+const SHEET_SITES =
+  "https://docs.google.com/spreadsheets/d/1GCInDCLc4h7xGekTAfAPeeY1vt0Gcv464dSLJi5MiAA/gviz/tq?tqx=out:csv&sheet=площадки";
+
+async function readSites() {
+  const res = await fetch(SHEET_SITES);
+  const csv = await res.text();
+
+  const lines = csv.trim().split("\n");
+  lines.shift();
+
+  return lines
+    .map(l => l.split(",")[0].replace(/"/g, "").trim())
+    .filter(Boolean);
+}
